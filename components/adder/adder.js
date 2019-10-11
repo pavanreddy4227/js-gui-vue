f5fafa26780c8a3e33e22b36ee9b855210d18848
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2019-10-11 13:12:03 -0500

    dfghj

--- components/adder/adder.js
+++ components/adder/adder.js
@@ -3,7 +3,6 @@ const add = (x, y) => { return x * y }
 const adder = new Vue({
   el: '#adder',
   data: {
-    guest: 'Emmett',
     firstNumber: 5,
     secondNumber: 3
   },
@@ -11,7 +10,7 @@ const adder = new Vue({
     result: function () {
       const i = parseInt(this.firstNumber)
       const j = parseInt(this.secondNumber)
-      return `${this.guest}, your sum is ${add(i, j)}.`
+      return `countOfStories* avgTasksPerStory = ${add(i, j)}.`
     }
   }
 })
